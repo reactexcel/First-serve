21d5f01610cebb6ea0df05a9d3d8cccabcefb006
manish <arun.etech2011@gmail.com>
2017-10-07 17:25:41 +0530

    helper  for restaurant lisner in admin page

--- src/views/admin_home.js
+++ src/views/admin_home.js
@@ -23,7 +23,9 @@ import Database from "../firebase/database";
 import RestaurantListItem from "./restaurant_list_item"
 import DismissKeyboard from "dismissKeyboard";
 import * as firebase from "firebase";
+import * as Helper from '../helper/helper';
 
+console.log(Helper);
 class AdminHome extends Component {
     static navigationOptions = {
         title: 'Restaurant',
@@ -232,48 +234,13 @@ class AdminHome extends Component {
 
     listenForRestaurants(restaurantRef) {
       // listen for changes to the tasks reference, when it updates we'll get a
-      // dataSnapshot from firebase
-      restaurantRef.on('value', (dataSnapshot) => {
-        // transform the children to an array
-        var restaurants = [];
-        var count = 0;
-        restaurants.push({isAddButton: true});
-        dataSnapshot.forEach((child) => {
-          child.forEach((ch) => {
-            var images = [];
-            if(ch.val().images !== undefined){
-              for (var key in ch.val().images) {
-                var img = ch.val().images[key];
-                images.splice((img.primary ? 0 : images.length), 0, {
-                  imageUrl: img.imageUrl, storageId: key, primary: img.primary, fileName: img.fileName, uid: child.key, restaurantId: ch.key
-                });
-              }
-            }
-
-            restaurants.splice((restaurants.length), 0, {
-              name: (ch.val().name ? ch.val().name : ''),
-              type: (ch.val().type ? ch.val().type : ''),
-              phone_number: (ch.val().phone_number ? ch.val().phone_number : ''),
-              short_description: (ch.val().short_description ? ch.val().short_description : ''),
-              long_description: (ch.val().long_description ? ch.val().long_description : ''),
-              booking_message: (ch.val().booking_message ? ch.val().booking_message : ''),
-              address: (ch.val().address ? ch.val().address : ''),
-              website_url: (ch.val().website_url ? ch.val().website_url : ''),
-              booking_url: (ch.val().booking_url ? ch.val().booking_url : ''),
-              instagram_url: (ch.val().instagram_url ? ch.val().instagram_url : ''),
-              fully_booked: ch.val().fully_booked,
-              images: images,
-              _uid: child.key,
-              _key: ch.key
-            });
-          });
-        });
-
+      // dataSnapshot from firebase with a helper function
+      Helper.listenRestaurants(restaurantRef).then((restaurants)=>{
         this.setState({
           restaurants: restaurants,
           dataSource: this.state.dataSource.cloneWithRows(restaurants)
         });
-      });
+      })
     }
 }
 
